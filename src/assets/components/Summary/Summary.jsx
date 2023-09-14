ad9972a77931fa1f3d6870cec44e91be965efca1
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-09-15 00:12:06 +0600

    now name is working

--- src/assets/components/Summary/Summary.jsx
+++ src/assets/components/Summary/Summary.jsx
@@ -1,13 +1,19 @@
 
 export default function Summary(data) {
-    const {allname}=data
+    const {id,allname}=data
   return (
     <div className="w-1/3 mx-5 p-5 bg-white rounded-lg text-left">
         <h1 className="text-xl font-semibold pb-2 text-blue-300">Credit Hour Remaining {data.allhour} hr</h1>
         <hr/>
         <h1 className="text-xl font-bold py-2 text-[#1C1B1B]">Course Name</h1>
         <hr/>
-            
+            {
+                allname.map((x)=>{
+                    return(
+                        <h2 key={id}>{x}</h2>
+                    )
+                })
+            }
         <hr/>
         <h1 className="py-2 font-semibold opacity-80">Total Credit Hour {data.allhour}</h1>
         <hr/>
